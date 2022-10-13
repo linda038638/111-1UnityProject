4ad684911a2edf8df734fdb92c6aadefa9e554e1
Snow.fishb <72215312+linda038638@users.noreply.github.com>
2022-10-13 18:17:42 +0800

    FirstScript
    創建第一支腳本

--- 0929game1/Assets/Script/FirstScript.cs
+++ 0929game1/Assets/Script/FirstScript.cs
@@ -1,7 +1,40 @@
+using System;
+
 using UnityEngine;
+/// <summary>
+/// KID老師的unity寫法與教學
+/// </summary>
+/// 
 
-public class FirstScript : MonoBehaviour
+#region 註解用法與介紹
+
+//單行註解
+/*多行註解
+ * 每enter一次
+ * 就多一個星號
+ * 記得在最後一行加入斜線
+ */
+
+#endregion
+
+namespace FirstGame
 {
+    public class FirstScript : MonoBehaviour
+    {
+        //遊戲預備
+        private void Awake()
+        {
+            print(0);
+            Debug.Log("哈囉");
+            Console.WriteLine("ji3u.3");
+        }
 
+        //遊戲開啟
+        private void Start()
+        {
+            
+        }
+
+    }
 
 }
